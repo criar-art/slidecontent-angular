c700f34b5a7bf9344fee2ab4350fc075ce7de7b1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 05:27:09 -0300

    refatory with isLanguageActive

--- src/app/components/base/app-language/app-language.component.ts
+++ src/app/components/base/app-language/app-language.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from '../../../services/language.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-language',
@@ -9,12 +10,17 @@ import { LanguageService } from '../../../services/language.service';
 export class AppLanguageComponent implements OnInit {
   languages = ['pt-BR', 'en-US', 'es-ES', 'fr-FR', 'ru-RU', 'zh-CN'];
 
-  constructor(private languageService: LanguageService) {
+  constructor(private languageService: LanguageService, private location: Location) {
   }
 
   ngOnInit(): void {
     const lang = this.languageService.getLanguage();
     this.languageService.setLanguage(lang);
+  }
+
+  isLanguageActive(language: string): boolean {
+    const path = this.location.path();
+    return path.startsWith(`/${language.slice(0, 2)}`);
   }
 
   getUrl(language: string): string {
