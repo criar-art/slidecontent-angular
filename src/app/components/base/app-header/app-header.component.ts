b0b6b22f1bb25eb5d84ae49b413d5f039f67ad28
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 03:34:33 -0300

    remove old input for title

--- src/app/components/base/app-header/app-header.component.ts
+++ src/app/components/base/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent implements OnInit {
-
-  @Input()  title: string;
 
   constructor() { }
 
