d7805ac6a1068346818297a8601bdce3ea3c2345
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 05:17:26 -0300

    use routerLinkActive in language

--- src/app/components/base/app-language/app-language.component.ts
+++ src/app/components/base/app-language/app-language.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from '../../../services/language.service';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-language',
@@ -10,30 +8,13 @@ import { filter } from 'rxjs/operators';
 })
 export class AppLanguageComponent implements OnInit {
   languages = ['pt-BR', 'en-US', 'es-ES', 'fr-FR', 'ru-RU', 'zh-CN'];
-  codeLanguage: string;
 
-  constructor(private languageService: LanguageService, private router: Router, private route: ActivatedRoute) {
-    // Monitora as mudanças de rota para atualizar o código de idioma
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      // Obtém o código de idioma da rota atual
-      const segments = this.route.snapshot.url.map(segment => segment.path);
-      if (segments.length > 0) {
-        this.codeLanguage = segments[0]; // Assume que o código de idioma está no primeiro segmento
-      } else {
-        this.codeLanguage = 'pt'; // Código de idioma padrão se não estiver presente na URL
-      }
-    });
+  constructor(private languageService: LanguageService) {
   }
 
   ngOnInit(): void {
     const lang = this.languageService.getLanguage();
     this.languageService.setLanguage(lang);
-  }
-
-  isLanguageValid(code: string): boolean {
-    return this.codeLanguage == code.slice(0, 2);
   }
 
   getUrl(language: string): string {
