2755a02d9cdb6afd1fc0eeb0ac4aa107ebd2e17f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-22 05:09:14 -0300

    add slideId

--- src/app/components/slide-item/slide-item.component.ts
+++ src/app/components/slide-item/slide-item.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import * as uuid from "uuid";
 
 @Component({
-  selector: 'app-slide-item',
-  templateUrl: './slide-item.component.html',
-  styleUrls: ['./slide-item.component.scss']
+  selector: "app-slide-item",
+  templateUrl: "./slide-item.component.html",
+  styleUrls: ["./slide-item.component.scss"],
 })
 export class SlideItemComponent implements OnInit {
-  @Input('actived') actived: boolean;
-  @Input('title') title: string;
-  @Input('img') img: string;
-  @Input('url') url: string;
-  @Input('target') target: string;
+  @Input("actived") actived: boolean;
+  @Input("title") title: string;
+  @Input("img") img: string;
+  @Input("url") url: string;
+  @Input("target") target: string;
+  slideId: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.slideId = "slide-id-" + uuid.v4();
   }
 
+  ngOnInit() {}
 }
