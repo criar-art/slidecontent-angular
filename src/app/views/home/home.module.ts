fa7bfde2bf7c6f7a74c7aa586a2b4bf42edcf355
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-05 02:13:14 -0300

    update version 1.5.0 and break change imports names

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,20 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeComponent } from './home.component';
+import { HomeComponent } from "./home.component";
 
-import { HomeRoutingModule } from './home-routing.module';
-// import { SlideModule } from '../../../../projects/slidecontent-angular/src/public-api';
-import { SlideModule } from 'slidecontent-angular';
+import { HomeRoutingModule } from "./home-routing.module";
+// import { SlideContentAngular } from '../../../../projects/slidecontent-angular/src/public-api';
+import { SlideContentAngular } from "slidecontent-angular";
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    SlideModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [SlideContentAngular, CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
